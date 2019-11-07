b49763bbcf29ce2e8f62ab5ed433cc4040c52266
MauriMatamoros <mauricio.enrique.matamoros@gmail.com>
2019-11-06 19:00:05 -0600

    made types dynamic

--- src/components/Index/ArraysCategory.js
+++ src/components/Index/ArraysCategory.js
@@ -4,135 +4,93 @@ import { MDBBtn } from 'mdbreact'
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos'
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos'
 import ItemsCarousel from 'react-items-carousel'
+import axios from 'axios'
 
 import CardCategory from './CardCategoty'
-
-const arraysFlowers = [
-  {
-    id: '1',
-    image:
-      'http://3.bp.blogspot.com/-EHQ7m2jUFIk/VGY6WLi1o8I/AAAAAAAAQc4/MWwFtJQcyYA/s1600/La%2BGazania%2B01.JPG',
-    title: 'Titulo de la categoria 1'
-  },
-  {
-    id: '2',
-    image:
-      'http://3.bp.blogspot.com/-EHQ7m2jUFIk/VGY6WLi1o8I/AAAAAAAAQc4/MWwFtJQcyYA/s1600/La%2BGazania%2B01.JPG',
-    title: 'Titulo de la categoria 2'
-  },
-  {
-    id: '3',
-    image:
-      'http://3.bp.blogspot.com/-EHQ7m2jUFIk/VGY6WLi1o8I/AAAAAAAAQc4/MWwFtJQcyYA/s1600/La%2BGazania%2B01.JPG',
-    title: 'Titulo de la categoria 3'
-  },
-  {
-    id: '4',
-    image:
-      'http://3.bp.blogspot.com/-EHQ7m2jUFIk/VGY6WLi1o8I/AAAAAAAAQc4/MWwFtJQcyYA/s1600/La%2BGazania%2B01.JPG',
-    title: 'Titulo de la categoria 4'
-  },
-  {
-    id: '5',
-    image:
-      'https://s1.significados.com/foto/photo-credit-dora-alis-on-visualhunt_bg.png',
-    title: 'Titulo de la categoria 5'
-  },
-  {
-    id: '6',
-    image:
-      'https://s1.significados.com/foto/photo-credit-dora-alis-on-visualhunt_bg.png',
-    title: 'Titulo de la categoria 6'
-  },
-  {
-    id: '7',
-    image:
-      'https://s1.significados.com/foto/photo-credit-dora-alis-on-visualhunt_bg.png',
-    title: 'Titulo de la categoria 7'
-  },
-  {
-    id: '8',
-    image:
-      'https://s1.significados.com/foto/photo-credit-dora-alis-on-visualhunt_bg.png',
-    title: 'Titulo de la categoria 8'
-  },
-  {
-    id: '9',
-    image:
-      'https://s1.significados.com/foto/photo-credit-dora-alis-on-visualhunt_bg.png',
-    title: 'Titulo de la categoria 9'
-  },
-  {
-    id: '10',
-    image:
-      'https://s1.significados.com/foto/photo-credit-dora-alis-on-visualhunt_bg.png',
-    title: 'Titulo de la categoria 10'
-  }
-]
+import Spinner from '../Spinner/Spinner'
+import { storage } from '../../firebase'
 
 class ArraysCategory extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      index: 0,
-      responsive: 5
-    }
-  }
+	constructor() {
+		super()
+		this.state = {
+			index: 0,
+			responsive: 5,
+			loading: true,
+			arraysFlowers: []
+		}
+	}
 
-  _prev = () => {
-    this.setState({ index: this.state.index - 1 })
-  }
-  _next = () => {
-    this.setState({ index: this.state.index + 1 })
-  }
+	_prev = () => {
+		this.setState({ index: this.state.index - 1 })
+	}
+	_next = () => {
+		this.setState({ index: this.state.index + 1 })
+	}
 
-  render() {
-    const chevronWidth = 40
-    return (
-      <>
-        <Container fluid className='pt-5 pb-5'>
-          <div className='text-center pb-5'>
-            <h1 className='font-weight-bold'>Lo que buscas</h1>
-          </div>
-        </Container>
-        <div style={{ padding: `0 ${chevronWidth}px` }}>
-          <ItemsCarousel
-            requestToChangeActive={() => console.log('Hola')}
-            activeItemIndex={this.state.index}
-            /* numberOfCards es la propiedad donde se agrupara los elementos en el responsive */
-            numberOfCards={this.state.responsive}
-            gutter={20}
-            leftChevron={
-              <MDBBtn
-                onClick={() => this._prev()}
-                size='lg'
-                rounded
-                color='black'
-              >
-                <ArrowBackIosIcon />
-              </MDBBtn>
-            }
-            rightChevron={
-              <MDBBtn
-                onClick={() => this._next()}
-                size='lg'
-                color='black'
-                rounded
-              >
-                <ArrowForwardIosIcon />
-              </MDBBtn>
-            }
-            outsideChevron
-            chevronWidth={chevronWidth}
-          >
-            {arraysFlowers.map(array => (
-              <CardCategory key={array.id} array={array} />
-            ))}
-          </ItemsCarousel>
-        </div>
-      </>
-    )
-  }
+	async componentDidMount() {
+		const { data } = await axios.post(
+			'http://localhost:5001/floristeria-cra/us-central1/getTypes'
+		)
+		const flowerArray = []
+		for (const type of data) {
+			const image = await storage
+				.ref(`types/${type.id}-${type.image}`)
+				.getDownloadURL()
+			flowerArray.push({ ...type, image })
+		}
+		this.setState(() => ({ loading: false, arraysFlowers: flowerArray }))
+	}
+
+	render() {
+		const chevronWidth = 40
+		return this.state.loading ? (
+			<Spinner />
+		) : (
+			<>
+				<Container fluid className='pt-5 pb-5'>
+					<div className='text-center pb-5'>
+						<h1 className='font-weight-bold'>Lo que buscas</h1>
+					</div>
+				</Container>
+				<div style={{ padding: `0 ${chevronWidth}px` }}>
+					<ItemsCarousel
+						requestToChangeActive={() => console.log('Hola')}
+						activeItemIndex={this.state.index}
+						/* numberOfCards es la propiedad donde se agrupara los elementos en el responsive */
+						numberOfCards={this.state.responsive}
+						gutter={20}
+						leftChevron={
+							<MDBBtn
+								onClick={() => this._prev()}
+								size='lg'
+								rounded
+								color='black'
+							>
+								<ArrowBackIosIcon />
+							</MDBBtn>
+						}
+						rightChevron={
+							<MDBBtn
+								onClick={() => this._next()}
+								size='lg'
+								color='black'
+								rounded
+							>
+								<ArrowForwardIosIcon />
+							</MDBBtn>
+						}
+						outsideChevron
+						chevronWidth={chevronWidth}
+					>
+						{this.state.arraysFlowers.map((array) => (
+							<CardCategory key={array.id} array={array} />
+						))}
+					</ItemsCarousel>
+				</div>
+			</>
+		)
+	}
 }
 
 export default ArraysCategory
