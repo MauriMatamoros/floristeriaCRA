d85e66bf5560cf61c151b1dea7c9c8e9eba059c2
Christian Daniel Najera <chris_najera@unitec.edu>
2019-11-17 18:42:30 -0600

    Update email profile

--- functions/index.js
+++ functions/index.js
@@ -1,207 +1,211 @@
-const functions = require('firebase-functions')
-const admin = require('firebase-admin')
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-const serviceAccount = require('./floristeria-cra-firebase-adminsdk-pdx5g-d28dccdf63.json')
+const serviceAccount = require("./floristeria-cra-firebase-adminsdk-pdx5g-d28dccdf63.json");
 
-const cors = require('cors')({ origin: true })
+const cors = require("cors")({origin: true});
 
 admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount),
-	databaseURL: 'https://floristeria-cra.firebaseio.com'
-})
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://floristeria-cra.firebaseio.com"
+});
 
-let db = admin.firestore()
+let db = admin.firestore();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.createProfile = functions.https.onRequest((req, res) =>
-	cors(req, res, async () => {
-		try {
-			const { uid, name, lastName, birthday, gender } = req.body
-			await db
-				.collection('users')
-				.doc(uid)
-				.set({ name, lastName, birthday, gender, role: 'client' })
-			res.status(201).send('User profile created.')
-		} catch (error) {
-			res.status(500).send('Server Error')
-		}
-	})
-)
+  cors(req, res, async () => {
+    try {
+      const {uid, name, lastName, birthday, gender} = req.body;
+      await db
+        .collection("users")
+        .doc(uid)
+        .set({name, lastName, birthday, gender, role: "client"});
+      res.status(201).send("User profile created.");
+    } catch (error) {
+      res.status(500).send("Server Error");
+    }
+  })
+);
 
 exports.getProfile = functions.https.onRequest((req, res) =>
-	cors(req, res, async () => {
-		try {
-			const { token } = req.body
-			const { uid } = await admin.auth().verifyIdToken(token)
-			const user = await db
-				.collection('users')
-				.doc(uid)
-				.get()
-			res.status(200).json(user.data())
-		} catch (error) {
-			res.status(500).send('Server Error')
-		}
-	})
-)
+  cors(req, res, async () => {
+    try {
+      const {token} = req.body;
+      const {uid} = await admin.auth().verifyIdToken(token);
+      const user = await db
+        .collection("users")
+        .doc(uid)
+        .get();
+      res.status(200).json(user.data());
+    } catch (error) {
+      res.status(500).send("Server Error");
+    }
+  })
+);
 
 exports.updateProfile = functions.https.onRequest((req, res) =>
-	cors(req, res, async () => {
-		try {
-			const { token, name, lastName, birthday, gender } = req.body
-			const { uid } = await admin.auth().verifyIdToken(token)
-			await db
-				.collection('users')
-				.doc(uid)
-				.update({ name, lastName, birthday, gender })
-			const user = await db
-				.collection('users')
-				.doc(uid)
-				.get()
-			res.status(200).json(user.data())
-		} catch (error) {
-			res.status(500).send('Server Error')
-		}
-	})
-)
+  cors(req, res, async () => {
+    try {
+      const {token, name, lastName, birthday, gender, email} = req.body;
+      const {uid} = await admin.auth().verifyIdToken(token);
+      await admin.auth().updateUser(uid, {
+        email: email,
+        emailVerified: false
+      });
+      await db
+        .collection("users")
+        .doc(uid)
+        .update({name, lastName, birthday, gender});
+      const user = await db
+        .collection("users")
+        .doc(uid)
+        .get();
+      res.status(200).json(user.data());
+    } catch (error) {
+      res.status(500).send("Server Error");
+    }
+  })
+);
 
 exports.postAddress = functions.https.onRequest((req, res) =>
-	cors(req, res, async () => {
-		try {
-			const { token, name, description } = req.body
-			const { uid } = await admin.auth().verifyIdToken(token)
+  cors(req, res, async () => {
+    try {
+      const {token, name, description} = req.body;
+      const {uid} = await admin.auth().verifyIdToken(token);
 
-			if (!uid) {
-				res.status(403).send('Please log in again.')
-			}
+      if (!uid) {
+        res.status(403).send("Please log in again.");
+      }
 
-			const { id } = await db
-				.collection('addresses')
-				.add({ name, description, owner: uid })
+      const {id} = await db
+        .collection("addresses")
+        .add({name, description, owner: uid});
 
-			const address = await db
-				.collection('addresses')
-				.doc(id)
-				.get()
+      const address = await db
+        .collection("addresses")
+        .doc(id)
+        .get();
 
-			res.status(201).json({ id, ...address.data() })
-		} catch (error) {
-			console.error(error)
-			res.status(500).send('Server Error')
-		}
-	})
-)
+      res.status(201).json({id, ...address.data()});
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Server Error");
+    }
+  })
+);
 
 exports.getAddresses = functions.https.onRequest((req, res) =>
-	cors(req, res, async () => {
-		try {
-			const { token } = req.body
-			const { uid } = await admin.auth().verifyIdToken(token)
-			if (!uid) {
-				res.status(403).send('Please log in again.')
-			}
-			const snapshots = await db
-				.collection('addresses')
-				.where('owner', '==', `${uid}`)
-				.get()
-			const addresses = []
-			if (snapshots.empty) {
-				res.status(200).json([])
-			}
-			snapshots.forEach((address) =>
-				addresses.push({
-					id: address.id,
-					...address.data()
-				})
-			)
-			res.status(200).json(addresses)
-		} catch (error) {
-			res.status(500).send('Server Error')
-		}
-	})
-)
+  cors(req, res, async () => {
+    try {
+      const {token} = req.body;
+      const {uid} = await admin.auth().verifyIdToken(token);
+      if (!uid) {
+        res.status(403).send("Please log in again.");
+      }
+      const snapshots = await db
+        .collection("addresses")
+        .where("owner", "==", `${uid}`)
+        .get();
+      const addresses = [];
+      if (snapshots.empty) {
+        res.status(200).json([]);
+      }
+      snapshots.forEach(address =>
+        addresses.push({
+          id: address.id,
+          ...address.data()
+        })
+      );
+      res.status(200).json(addresses);
+    } catch (error) {
+      res.status(500).send("Server Error");
+    }
+  })
+);
 
 exports.removeAddress = functions.https.onRequest((req, res) =>
-	cors(req, res, async () => {
-		try {
-			const { token, id } = req.body
-			const { uid } = await admin.auth().verifyIdToken(token)
-			if (!uid) {
-				res.status(403).send('Please log in again.')
-			}
-			await db
-				.collection('addresses')
-				.doc(id)
-				.delete()
-			res.status(200).json(id)
-		} catch (error) {
-			res.status(500).send('Server Error')
-		}
-	})
-)
+  cors(req, res, async () => {
+    try {
+      const {token, id} = req.body;
+      const {uid} = await admin.auth().verifyIdToken(token);
+      if (!uid) {
+        res.status(403).send("Please log in again.");
+      }
+      await db
+        .collection("addresses")
+        .doc(id)
+        .delete();
+      res.status(200).json(id);
+    } catch (error) {
+      res.status(500).send("Server Error");
+    }
+  })
+);
 
 exports.postType = functions.https.onRequest((req, res) =>
-	cors(req, res, async () => {
-		try {
-			const { token, name, image } = req.body
-			const { uid } = await admin.auth().verifyIdToken(token)
+  cors(req, res, async () => {
+    try {
+      const {token, name, image} = req.body;
+      const {uid} = await admin.auth().verifyIdToken(token);
 
-			if (!uid) {
-				res.status(403).send('Please log in again.')
-			}
+      if (!uid) {
+        res.status(403).send("Please log in again.");
+      }
 
-			const { id } = await db.collection('types').add({ name, image })
+      const {id} = await db.collection("types").add({name, image});
 
-			const type = await db
-				.collection('types')
-				.doc(id)
-				.get()
+      const type = await db
+        .collection("types")
+        .doc(id)
+        .get();
 
-			res.status(201).json({ id, ...type.data() })
-		} catch (error) {
-			console.error(error)
-			res.status(500).send('Server Error')
-		}
-	})
-)
+      res.status(201).json({id, ...type.data()});
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Server Error");
+    }
+  })
+);
 
 exports.getTypes = functions.https.onRequest((req, res) =>
-	cors(req, res, async () => {
-		try {
-			const snapshots = await db.collection('types').get()
-			const types = []
-			if (snapshots.empty) {
-				res.status(200).send([])
-			}
-			snapshots.forEach((type) =>
-				types.push({
-					id: type.id,
-					...type.data()
-				})
-			)
-			res.status(200).json(types)
-		} catch (error) {
-			res.status(500).send('Server Error')
-		}
-	})
-)
+  cors(req, res, async () => {
+    try {
+      const snapshots = await db.collection("types").get();
+      const types = [];
+      if (snapshots.empty) {
+        res.status(200).send([]);
+      }
+      snapshots.forEach(type =>
+        types.push({
+          id: type.id,
+          ...type.data()
+        })
+      );
+      res.status(200).json(types);
+    } catch (error) {
+      res.status(500).send("Server Error");
+    }
+  })
+);
 
 exports.removeType = functions.https.onRequest((req, res) =>
-	cors(req, res, async () => {
-		try {
-			const { token, id } = req.body
-			const { uid } = await admin.auth().verifyIdToken(token)
-			if (!uid) {
-				res.status(403).send('Please log in again.')
-			}
-			await db
-				.collection('types')
-				.doc(id)
-				.delete()
-			res.status(200).json(id)
-		} catch (error) {
-			res.status(500).send('Server Error')
-		}
-	})
-)
+  cors(req, res, async () => {
+    try {
+      const {token, id} = req.body;
+      const {uid} = await admin.auth().verifyIdToken(token);
+      if (!uid) {
+        res.status(403).send("Please log in again.");
+      }
+      await db
+        .collection("types")
+        .doc(id)
+        .delete();
+      res.status(200).json(id);
+    } catch (error) {
+      res.status(500).send("Server Error");
+    }
+  })
+);
--- src/components/Account/AccountHeader.js
+++ src/components/Account/AccountHeader.js
@@ -1,66 +1,65 @@
-import React from 'react'
-import { Segment } from 'semantic-ui-react'
+import React from "react";
+import {Segment} from "semantic-ui-react";
 
 const AccountHeader = props => {
-  const { name, lastName, birthday, gender } = props.profile
+  const {name, lastName, birthday, gender} = props.profile;
+  const {email} = props.auth;
   return (
     <>
-      <Segment secondary inverted color='brown'>
-        <div className='d-flex justify-content-center'>
-          <img src='./assets/logo.png' style={styles.imgLogo} />
+      <Segment secondary inverted color="brown">
+        <div className="d-flex justify-content-center">
+          <img src="./assets/logo.png" style={styles.imgLogo} />
         </div>
-        <div className='container text-center mt-3 mb-4'>
-          <div className='row mt-3'>
-            <div className='col-lg-6 col-sm -12'>
-              <p className='h5 font-weight-normal text-lg-right'>Nombre:</p>
+        <div className="container text-center mt-3 mb-4">
+          <div className="row mt-3">
+            <div className="col-lg-6 col-sm -12">
+              <p className="h5 font-weight-normal text-lg-right">Nombre:</p>
             </div>
-            <div className='col-lg-6 col-sm -12'>
-              <p className='h5 font-weight-normal text-lg-left'>
+            <div className="col-lg-6 col-sm -12">
+              <p className="h5 font-weight-normal text-lg-left">
                 {name} {lastName}
               </p>
             </div>
           </div>
-          <div className='row mt-3'>
-            <div className='col-lg-6 col-sm -12'>
-              <p className='h5 font-weight-normal text-lg-right'>Correo:</p>
+          <div className="row mt-3">
+            <div className="col-lg-6 col-sm -12">
+              <p className="h5 font-weight-normal text-lg-right">Correo:</p>
             </div>
-            <div className='col-lg-6 col-sm -12'>
-              <p className='h5 font-weight-normal text-lg-left'>
-                correo electronico
-              </p>
+            <div className="col-lg-6 col-sm -12">
+              <p className="h5 font-weight-normal text-lg-left">{email}</p>
             </div>
           </div>
-          <div className='row mt-3'>
-            <div className='col-lg-6 col-sm -12'>
-              <p className='h5 font-weight-normal text-lg-right'>
+          <div className="row mt-3">
+            <div className="col-lg-6 col-sm -12">
+              <p className="h5 font-weight-normal text-lg-right">
                 Fecha de nacimiento:
               </p>
             </div>
-            <div className='col-lg-6 col-sm -12'>
-              <p className='h5 font-weight-normal text-lg-left'>{birthday}</p>
+            <div className="col-lg-6 col-sm -12">
+              <p className="h5 font-weight-normal text-lg-left">{birthday}</p>
             </div>
           </div>
-          <div className='row mt-3'>
-            <div className='col-lg-6 col-sm -12'>
-              <p className='h5 font-weight-normal text-lg-right'>Género:</p>
+          <div className="row mt-3">
+            <div className="col-lg-6 col-sm -12">
+              <p className="h5 font-weight-normal text-lg-right">Género:</p>
             </div>
-            <div className='col-lg-6 col-sm -12'>
-              <p className='h5 font-weight-normal text-lg-left'>
-                {gender === 'male' ? 'Hombre' : 'Mujer'}
+            <div className="col-lg-6 col-sm -12">
+              <p className="h5 font-weight-normal text-lg-left">
+                {gender === "male" ? "Hombre" : "Mujer"}
               </p>
             </div>
           </div>
         </div>
       </Segment>
     </>
-  )
-}
+  );
+};
 
 const styles = {
   imgLogo: {
-    width: '45%',
-    height: '35%'
+    width: "45%",
+    height: "35%"
   }
-}
+};
 
-export default AccountHeader
+export default AccountHeader;
