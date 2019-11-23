e73e53b23723eace4e3f3727bd09bddd415882b7
Christian Daniel Najera <chris_najera@unitec.edu>
2019-11-23 15:52:08 -0600

    listado de productos por type

--- src/components/Products/ProductItem.js
+++ src/components/Products/ProductItem.js
@@ -1,26 +1,39 @@
-import React from "react";
+import React, {Component} from "react";
 import {Card, Button} from "semantic-ui-react";
+import {firebaseConnect} from "react-redux-firebase";
 
-const ProductItem = ({name, price, type, description}) => {
-  return (
-    <Card>
-      <Card.Content>
-        <Card.Header>{name}</Card.Header>
-        <Card.Meta>{price}</Card.Meta>
-        <Card.Description>{description}</Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <div className="ui two buttons">
-          <Button basic color="green">
-            Edit
-          </Button>
-          <Button basic color="red">
-            Delete
-          </Button>
-        </div>
-      </Card.Content>
-    </Card>
-  );
-};
+class ProductItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: ""
+    };
+  }
+  async componentDidMount() {
+    const {firebase, id, images} = this.props;
+    let productImage = await firebase
+      .storage()
+      .ref(`products/${id}-${images[0]}`)
+      .getDownloadURL();
 
-export default ProductItem;
+    if (productImage) {
+      this.setState({
+        image: productImage
+      });
+    }
+  }
+  render() {
+    let {name, price, description} = this.props;
+    return (
+      <Card
+        fluid
+        image={this.state.image}
+        title={name}
+        meta={`LPS ${price}`}
+        description={description}
+      />
+    );
+  }
+}
+
+export default firebaseConnect()(ProductItem);
