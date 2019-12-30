00738fc4ac3186718a8a4c03f0383f3917f8a056
Marvin Ramirez <mramirez@yourappland.com>
2019-12-30 05:51:37 -0600

    Se ha modificado el estilo de las noticias

--- src/components/Blogs/ItemBlogSmall.js
+++ src/components/Blogs/ItemBlogSmall.js
@@ -1,60 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import ModalBlog from '../Blogs/ModalBlog'
 
 const ItemBlogSmall = props => {
+  const [visible, setVisible] = useState(false)
+
   return (
     <>
-      <div className='container'>
-        <div className='row' style={styles.container}>
-          <div className='col-lg-3 col-12 text-center'>
-            <div>
-              <p className='font-weight-bold'>Nuevo arreglo de San Valentin</p>
-            </div>
-            <div className='pb-4'>
-              <img
-                src='./assets/News/img-3.jpg'
-                style={styles.sizeImage}
-                alt='new'
-              />
-            </div>
+      <div className='col-lg-4 col-md-6 col-12 text-center pb-5'>
+        <Link onClick={() => setVisible(!visible)}>
+          <div className='pb-4'>
+            <img
+              src='./assets/News/img-3.jpg'
+              style={styles.sizeImage}
+              alt='new'
+            />
           </div>
-          <div className='p-4 col-lg-9 col-12'>
-            <Link className='text-decoration-none text-dark' to='/blog/new'>
-              <h4>
-                Disfruta de nuestro nuevo arreglo de San Valentin y totalmente
-                mejorado.
-              </h4>
-            </Link>
-            <p className='text-blue pb-3'>
-              Blanca | <span>02/12/2019 - 09:24 pm</span>
-            </p>
+          <div>
             <p style={styles.fontDescription}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
               nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
               reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
+              nulla pariatur.
             </p>
           </div>
-        </div>
-
-        <div></div>
+        </Link>
+        <ModalBlog openModal={visible} event={setVisible} />
       </div>
     </>
   )
 }
 
 const styles = {
-  container: {},
   sizeImage: {
-    width: 150,
-    height: 230
+    width: '90%',
+    height: 'auto',
+    borderRadius: 15
   },
   fontDescription: {
     fontSize: 15,
-    color: 'gray'
+    paddingLeft: '2rem',
+    paddingRight: '2rem',
+    color: 'gray',
+    fontWeight: 'bold'
   }
 }
 export default ItemBlogSmall
--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,22 +1,19 @@
 import React from 'react'
 import { Container } from 'semantic-ui-react'
-import ItemBlogBig from '../components/Blogs/ItemBlogBig'
 import ItemBlogSmall from '../components/Blogs/ItemBlogSmall'
 
 const Blog = props => {
   return (
     <>
       <Container className='mbt-10em'>
-        <ItemBlogBig />
-        <div className='container'>
-          <div className='row'>
-            <div className='col-lg-8 col-12'>
-              <ItemBlogSmall />
-              <ItemBlogSmall />
-              <ItemBlogSmall />
-            </div>
-            <div className='col-lg-4 col-12'>Video</div>
-          </div>
+        <div className='row'>
+          <ItemBlogSmall />
+          <ItemBlogSmall />
+          <ItemBlogSmall />
+          <ItemBlogSmall />
+          <ItemBlogSmall />
+          <ItemBlogSmall />
+          <ItemBlogSmall />
         </div>
       </Container>
     </>
