8f9c1d49797fadd32f9556a10739a83c16283688
Marvin Ramirez <mramirez@yourappland.com>
2019-12-30 04:34:29 -0600

    Se ha personalizado el estilo de carrito de compras

--- src/components/Cart/SideBarCart.js
+++ src/components/Cart/SideBarCart.js
@@ -15,32 +15,43 @@ const SideBarCart = props => {
         visible={props.opened}
         style={styles.containerSideBar}
       >
-        <div className='mt-2 pr-3 d-flex justify-content-end'>
+        <div className='d-flex flex-row justify-content-between pl-4 pr-4'>
+          <p style={styles.titleHeader} className='mt-4'>
+            CARRITO
+          </p>
           <button
             type='button'
-            className='btn text-dark'
+            className='btn btn-link text-dark'
             onClick={() => props.setOpened(false)}
           >
-            <i className='fas fa-times fa-1x' />
+            <p style={styles.titleHeader}>X</p>
           </button>
         </div>
 
-        {/* En caso de no existir producto en el carro de compras */}
-        <div className='text-center p-3'>
-          <img
-            src='./assets/cart.png'
-            style={{ width: 70, height: 50 }}
-            alt='cart'
-          />
-          <p style={{ color: 'gray' }}>
-            No tiene productos en el carro de compras, favor iniciar sesión y
-            agregar arreglos a su carrito.
+        <div className='pl-4 pb-2 pt-2' style={styles.containerPromo}>
+          <p className='text-left text-muted'>
+            Gaste L.100 más y obtenga envío gratis!
           </p>
         </div>
 
         <ItemCart />
         <ItemCart />
-        <ItemCart />
+
+        <div
+          className='pt-5 mt-5 text-left pl-4 pr-4'
+          style={styles.containerTop}
+        >
+          <textarea
+            style={styles.inputAddTitle}
+            placeholder='Añadir nota al pedido'
+          />
+          <p style={styles.sentTitle}>
+            Gastos de envio e impuestos calculados en la caja
+          </p>
+          <div className='mt-5 w-100 text-center'>
+            <button className='btn btn-dark'>Comprar anora</button>
+          </div>
+        </div>
       </Sidebar>
     </>
   )
@@ -49,7 +60,27 @@ const SideBarCart = props => {
 const styles = {
   containerSideBar: {
     backgroundColor: 'white',
-    width: '40%'
+    width: '30%'
+  },
+  titleHeader: { fontSize: 20 },
+  containerPromo: {
+    borderBottom: '1px solid lightgray',
+    borderTop: '1px solid lightgray'
+  },
+  containerTop: {
+    borderTop: '1px solid lightgray'
+  },
+  addTitle: {
+    color: 'gray'
+  },
+  sentTitle: {
+    color: 'gray',
+    fontSize: 13
+  },
+  inputAddTitle: {
+    border: 0,
+    textDecoration: 'none',
+    width: '100%'
   }
 }
 
