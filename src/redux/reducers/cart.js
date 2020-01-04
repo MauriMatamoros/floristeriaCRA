5de7157ec65fba715c56d6a6b0375518bedb5406
MauriMatamoros <mauricio.enrique.matamoros@gmail.com>
2020-01-04 03:06:38 -0600

    im not getting paid enough

---
+++ src/redux/reducers/cart.js
@@ -0,0 +1,17 @@
+import { ADD_PRODUCT, REMOVE_PRODUCT } from '../actions/types'
+
+const initialState = {
+	products: []
+}
+
+export default (state = initialState, action) => {
+	const { type, payload } = action
+	switch (type) {
+		case ADD_PRODUCT:
+			return { products: [...state.products, payload] }
+		case REMOVE_PRODUCT:
+			return state.products.filter(({ id }) => id !== payload)
+		default:
+			return state
+	}
+}
